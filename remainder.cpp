54b305e0631877b6a7b7583c2b4f526cae2b01c9
Nux <72265156+RetrO-13th@users.noreply.github.com>
2023-11-09 17:38:57 +0330

    Add remainder.cpp file

---
+++ remainder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int my_remainder(int a, int b)
+{
+    if ((a % 3 == 0 && b % 3 == 1) || (a % 3 == 0 && b % 3 == 0) || (a % 3 == 0 && b % 3 == 2))
+        return 0;
+    if ((a % 3 == 1 && b % 3 == 0) || (a % 3 == 1 && b % 3 == 1) || (a % 3 == 1 && b % 3 == 2) || (a % 3 == 2 && b % 3 == 0))
+        return 1;
+    if (a % 3 == 2 && b % 3 == 1)
+    {
+        if ((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && b % 2 == 0))
+            return 1;
+
+        return 2;
+    }
+    if (a % 3 == 2 && b % 3 == 2)
+    {
+        if ((a % 2 != 0 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 0))
+            return 1;
+
+        return 2;
+    }
+
+    return 0;
+}
+
+int main()
+{
+
+    int a, b;
+    cin >> a >> b;
+
+    int ans = my_remainder(a, b);
+    cout << ans;
+
+    return 0;
+}
